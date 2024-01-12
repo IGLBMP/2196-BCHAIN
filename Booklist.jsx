3cdf8533104b893b78e286d518a506f87850720e
IGLBMP <89353214+IGLBMP@users.noreply.github.com>
2024-01-12 14:32:52 +0800

    Add files via upload

---
+++ Booklist.jsx
@@ -0,0 +1,12 @@
+export default function Booklist() {
+    let message = "BookList";
+    return(
+        <div>
+             {<h3>{message}</h3> }
+            <p>The Diaries of José Rizal</p>
+            <p> El Filibusterismo </p>
+            <p>  Noli me tangere </p>
+            
+        </div>
+    ); 
+}
---
+++ MovieList.jsx
@@ -0,0 +1,11 @@
+export default function MovieList () {
+    return(
+        <div>
+             <h3>MovieList</h3> 
+            <p> Power Rangers </p>
+            <p>Power Puff Girls</p>
+            <p>Bob the Builder</p>
+            
+        </div>
+    ); 
+}
