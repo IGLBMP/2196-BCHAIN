4be36f6dd132903c32036bd4d06de755d244ef86
IGLBMP <89353214+IGLBMP@users.noreply.github.com>
2024-01-12 14:13:28 +0800

    Add files via upload

---
+++ MovieList.jsx
@@ -0,0 +1,11 @@
+export default function MovieList () {
+    return(
+        <div>
+             <h3>MovieList</h3> 
+            <p> Power Rangers </p>
+            <p>Barney the Dinosaur</p>
+            <p>Bob the Builder</p>
+            
+        </div>
+    ); 
+}
